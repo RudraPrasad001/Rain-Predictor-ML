d21446a8baf94fe615c50abfce2b21eb98ce6ae8
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-08-01 09:18:11 +0530

    Error handling

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from app.api import routes_query
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
 
 app.include_router(routes_query.router, prefix="/query", tags=["Query"])
+origins = [
+    "http://localhost:3000",  # React or web frontend
+    "http://127.0.0.1:3000",
+    # Add more if needed
+    "https://yourfrontend.com"  # production domain
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # OR ["*"] to allow all origins (not recommended for production)
+    allow_credentials=True,
+    allow_methods=["*"],              # Allows all HTTP methods: GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],              # Allows all headers
+)
